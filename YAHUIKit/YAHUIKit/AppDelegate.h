f271fa016b0adbaed0fb3f129a91f83136dbb73f
王时温 <yahua523@163.com>
2018-11-28 00:03:02 +0800

    添加工程代码

---
+++ YAHUIKit/YAHUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YAHUIKit
+//
+//  Created by wangshiwen on 2018/11/28.
+//  Copyright © 2018 yahua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YAHUIKit/YAHUIKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAHUIKit
+//
+//  Created by wangshiwen on 2018/11/28.
+//  Copyright © 2018 yahua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
